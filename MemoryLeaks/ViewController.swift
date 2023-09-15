b53abac26b58f52b9d54992bccfcc94d3d053ff1
Noor Ali Butt <noor@crosscode.dev>
2023-09-16 03:15:55 +0400

    Initial Commit

---
+++ MemoryLeaks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemoryLeaks
+//
+//  Created by Noor on 9/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
